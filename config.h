a6c650ab20be91dba10aac735e314fe0061d9ced
AxelBohm <axel.boehm@ymail.com>
2019-05-10 10:16:48 -0400

    inverted colors for better visibility

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*                fg         bg       */
 	[SchemeNorm] = { "#d8dee9", "#2e3440" },
-	[SchemeSel]  = { "#81a1c1", "#2e3440" },
+	[SchemeSel]  = { "#2e3440", "#81a1c1" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
