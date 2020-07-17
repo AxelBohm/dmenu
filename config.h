a481f564aa5b5fcced27f00fb4c5551bf6b13265
AxelBohm <axel.boehm@ymail.com>
2020-07-17 17:08:27 +0200

    sourcerer colors

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                fg         bg       */
-	[SchemeNorm] = { "#d8dee9", "#2e3440" },
-	[SchemeSel]  = { "#2e3440", "#81a1c1" },
+	[SchemeNorm] = { "#c2c2b0", "#222222" },
+	[SchemeSel]  = { "#222222", "#719611" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
