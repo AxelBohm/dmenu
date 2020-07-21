75b0316cae73507a56cde0e9b07961de8f9bb171
AxelBohm <axel.boehm@ymail.com>
2020-07-21 11:21:07 +0200

    sourcere theme

--- config.h
+++ config.h
@@ -10,9 +10,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#222222" },
-	[SchemeSel] = { "#98971a", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#c2c2b0", "#222222" },
+	[SchemeSel]  = { "#222222", "#719611" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
+};
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
